6480a570e2ff074d1696ba3eab08217b7284b210
Moreno Almeida <moreno.ferreira11@gmail.com>
2025-08-28 15:05:06 -0300

    ,

---
+++ src/Media.java
@@ -0,0 +1,20 @@
+public class Media {
+
+
+    public static void main(String[] args) {
+        mediaDasNotas();
+    }
+
+    public static void mediaDasNotas() {
+        int nota1 = 5;
+        int nota2 = 7;
+        int nota3 = 8;
+        int nota4 = 2;
+
+
+    int notaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A media final do aluno e: " + notaFinal);
+    }
+
+}
