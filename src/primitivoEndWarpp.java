60a203b1aa2bef4e33ffd1e76264bf1190007453
Moreno Almeida <moreno.ferreira11@gmail.com>
2025-08-28 21:04:51 -0300

    exercicio mondulo 9

---
+++ src/primitivoEndWarpp.java
@@ -0,0 +1,11 @@
+public class primitivoEndWarpp {
+
+    public static void main(String[] args) {
+
+        int idade = 10;
+        System.out.println("Primitivo:" + idade);
+
+        Integer idadeWrapper = 10;
+        System.out.println ("wrapper:" + idadeWrapper);
+    }
+}
