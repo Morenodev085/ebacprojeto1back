52117ff3b8b49a72ea793397b9096c622e2c9dbd
Moreno Almeida <moreno.ferreira11@gmail.com>
2025-08-28 21:18:36 -0300

    exercicio mondulo 9 2

--- src/primitivoEndWarpp.java
+++ src/primitivoEndWarpp.java
@@ -7,5 +7,17 @@ public class primitivoEndWarpp {
 
         Integer idadeWrapper = 10;
         System.out.println ("wrapper:" + idadeWrapper);
+
+        //Aqui eu converto para a Warpper
+        Integer idadeWrapperWrapper = Integer.valueOf(idade);
+
+        int numero = 50;
+
+         Integre Jnumerowapper = Integer.valueOf(numero) ;
+
+         int idadeDesempactada = idadeWrapper.intValue();
+
+         boolean ativo = true ;
+         Boolean ativoWrapper = true;
     }
 }
