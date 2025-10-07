f080d53545a36b0840e1811f3c5a3f10a7c3d3ff
Moreno Almeida <moreno.ferreira11@gmail.com>
2025-10-06 21:32:12 -0300

    ..

--- src/projetoLista/ListaPessoas.java
+++ src/projetoLista/ListaPessoas.java
@@ -1,6 +1,8 @@
 package projetoLista;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -29,8 +31,10 @@ public class ListaPessoas {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<Pessoa> listaGeral = new ArrayList<>();
         String continuar;
 
+        // Passo 1: Adicionar pessoas
         do {
             System.out.print("Digite o nome da pessoa: ");
             String nome = sc.nextLine();
@@ -38,31 +42,38 @@ public class ListaPessoas {
             System.out.print("Digite o sexo (masculino/feminino): ");
             String sexo = sc.nextLine();
 
-            Pessoa p = new Pessoa(nome, sexo);
-
-            if (sexo.equalsIgnoreCase("masculino")) {
-                masculino.add(p);
-            } else if (sexo.equalsIgnoreCase("feminino")) {
-                feminino.add(p);
-            } else {
-                System.out.println("Sexo inválido! Pessoa não adicionada.");
-            }
+            listaGeral.add(new Pessoa(nome, sexo));
 
             System.out.print("Deseja adicionar outra pessoa? (s/n): ");
             continuar = sc.nextLine();
 
         } while (continuar.equalsIgnoreCase("s"));
 
-        // Exibir listas
-        System.out.println("\nLista Masculina:");
-        for (Pessoa p : masculino) {
-            System.out.println(p.getNome());
+        // Passo 2: Ordenar a lista geral em ordem alfabética
+        Collections.sort(listaGeral, new Comparator<Pessoa>() {
+            @Override
+            public int compare(Pessoa p1, Pessoa p2) {
+                return p1.getNome().compareToIgnoreCase(p2.getNome());
+            }
+        });
+
+        // Passo 3: Separar em masculino e feminino
+        for (Pessoa p : listaGeral) {
+            if (p.getSexo().equalsIgnoreCase("masculino")) {
+                masculino.add(p);
+            } else if (p.getSexo().equalsIgnoreCase("feminino")) {
+                feminino.add(p);
+            } else {
+                System.out.println("Sexo inválido para " + p.getNome() + "!");
+            }
         }
 
-        System.out.println("\nLista Feminina:");
-        for (Pessoa p : feminino) {
-            System.out.println(p.getNome());
-        }
+        // Passo 4: Transformar listas em strings separadas por vírgula
+        String listaMasculino = String.join(", ", masculino.stream().map(Pessoa::getNome).toArray(String[]::new));
+        String listaFeminino = String.join(", ", feminino.stream().map(Pessoa::getNome).toArray(String[]::new));
+
+        System.out.println("\nLista Masculina: " + listaMasculino);
+        System.out.println("Lista Feminina: " + listaFeminino);
 
         sc.close();
     }
